9b60abf231f8757ecaf6bc73c7313b846e261278
GUO Xiaoqiang <guoxiaoq@gmail.com>
2019-08-13 15:23:56 +0800

    update

---
+++ publish.bat
@@ -0,0 +1,12 @@
+@echo off
+ECHO GIT STATUS
+git status
+ECHO GIT STATUS
+git status
+ECHO GIT commit
+git add -A
+git commit -m "update"
+ECHO GIT PUSH
+git push origin
+ECHO GIT STATUS
+git status
